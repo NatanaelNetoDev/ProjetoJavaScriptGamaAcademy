8c8fa20978840f2de4a8e72e3cc1c40188c5968f
Natanael Neto <natanaelneto.ti@gmail.com>
2021-07-07 00:13:34 -0300

    Projeto Busca Livros feito em curso

---
+++ database.js
@@ -0,0 +1,64 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Harry Potter e a pedra filosofal",
+        autor: "J.K Rowling",
+        categoria: "Aventura",
+        paginas: 264,
+        recomenda: true,
+        leu: true
+    },
+
+    {
+        id: 2,
+        nome: "Quem pensa, enriquece",
+        autor: "Napoleon Hill",
+        categoria: "DevPessoal",
+        paginas: 364,
+        recomenda: true,
+        leu: true
+    },
+
+    {
+        id: 3,
+        nome: "Percy Jackson e a Maldição do Titã",
+        autor: "Rick Riordan",
+        categoria: "Aventura",
+        paginas: 331,
+        recomenda: true,
+        leu: true
+    },
+
+    {
+        id: 4,
+        nome: "Os Heróis do Olimpo, A Casa de Hades ",
+        autor: "Rick Riordan",
+        categoria: "Aventura",
+        paginas: 448,
+        recomenda: true,
+        leu: true
+    },
+
+    {
+        id: 5,
+        nome: "As 48 Leis do Poder",
+        autor: "Robert Greene",
+        categoria: "DevPessoal",
+        paginas: 137,
+        recomenda: true,
+        leu: true
+    },
+
+    {
+        id: 6,
+        nome: "Magnus Chase e os Deuses de Asgard, O Martelo de Thor",
+        autor: "Rick Riordan",
+        categoria: "Aventura",
+        paginas: 400,
+        recomenda: true,
+        leu: true
+    },
+
+]
+
+module.exports = livros
